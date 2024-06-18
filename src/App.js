059cc3ea48df864b254ea45e37d2f163fa0bf20b
Kelvin Kibet <kelvinkibet@kelvins-mbp.myfiosgateway.com>
2024-06-18 16:16:30 -0400

    keeper app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Notes from './Notes';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Notes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Notes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Notes () {
+return (
+<div className="notes">
+    <h1>A TALE OF TWO CITIES</h1>
+    <p>“It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of light, it was the season of darkness, it was the spring of hope, it was the winter of despair.”</p>
+ </div>
+);
+};
+export default Notes 
